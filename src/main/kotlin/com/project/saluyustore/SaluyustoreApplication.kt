dcab0d624e9bfd5226b8cf1870702f89704f08e5
Muhamad Abdul Aziz <azizgimboell@gmail.com>
2023-05-17 21:00:34 +0700

    init

---
+++ src/main/kotlin/com/project/saluyustore/SaluyustoreApplication.kt
@@ -0,0 +1,11 @@
+package com.project.saluyustore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SaluyustoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<SaluyustoreApplication>(*args)
+}

6cf798a52464d2714ecc792b391630afe7248d8a
irsanr6 <irsanr6@gmail.com>
2023-05-20 17:42:02 +0700

    Update code generate user id

--- src/main/kotlin/com/project/saluyustore/repository/UserRepository.kt
+++ src/main/kotlin/com/project/saluyustore/repository/UserRepository.kt
@@ -2,9 +2,11 @@ package com.project.saluyustore.repository
 
 import com.project.saluyustore.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UserRepository : JpaRepository<User, String> {
-
+    @Query(value = "select func_gen_seq_user_id()", nativeQuery = true)
+    fun getSeqUserId(): String
 }
